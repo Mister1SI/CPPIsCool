8569c6ac9b8dde19b70592478ce0d0882597560d
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2021-03-20 11:50:49 -0500

    0.0.4: General updates.

--- CPPIsCool/Source.cpp
+++ CPPIsCool/Source.cpp
@@ -8,8 +8,8 @@ int randNum(int max) {
 }
 int main() {
 	int workers = 100, month = 1;
-	float totalCompanyValue = 101872.62, revenue = 6724.11, profitMargin = 0.34, wage = 9, potentialWage, potentialWage2, profit;
-	string version = "0.0.3", name, companyName, command = "";
+	float totalCompanyValue = 101872.62, revenue = 6724.11, profitMargin = 0.34, wage = 9, percentOutsourced = 0, potentialWage, potentialWage2, profit;
+	string version = "0.0.3", name, companyName, command = "", newline;
 	string randomEvents[] = { "deathAssassinated", "deathCancer", "deathCarCrash", "governmentGrant", "childLaborExposed"};
 	bool nextMonth = false;
 
@@ -19,15 +19,18 @@ int main() {
 	cin >> name;
 	cout << "Thank you. Please enter your company name: \n> ";
 	cin >> companyName;
-	cout << "Welcome, " << name << ", owner of " << companyName << ", to Supply chain. You are a new business owner toying with the idea of outsourcing, which is getting the materials for and creating ypur products in a different country.\n";
+	cout << "Welcome, " << name << ", owner of " << companyName << ", to Supply chain. You are a new business owner toying with the idea of outsourcing, which is getting the materials for and creating ypur products in a different country. This can lower costs, but it has some effects.\n";
+	cin >> newline;
+	cout << "The United States outsources a ton of stuff - in 2017, 84.2% of outsourcing deals came from the US!";
 	while (true) {
-		cout << "\n==========\nStart of month " << month << "\nTotal workers: " << workers << endl << "\nTotal comany value: $" << totalCompanyValue << "\nTotal revenue per month: $" << revenue << "\nProfit per month: $" << profit << "\nProfit margin: " << profitMargin * 100 << "%\n";
+		cout << "\n==========\nStart of month " << month << "\nTotal workers: " << workers << "\n\nTotal comany value: $" << totalCompanyValue << "\nTotal revenue per month: $" << revenue;
+		cout << "\nProfit per month: $" << profit << "\nProfit margin: " << profitMargin * 100 << "%\nPercent of products outsourced: " << percentOutsourced << "%\n";
 
 		while (true) {
 			cout << "Enter a command(help for a list): \n> ";
 			cin >> command;
 			if (command == "help") {
-				cout << "next - move to the next month\nhelp - show this list\nsc - show the supply chain\nwage - change the wage of your workers\nstop - end the game\n";
+				cout << "next - move to the next month\nhelp - show this list\nsc - show the supply chain\nwage - change the wage of your workers\noutsrc - edit your outsourcing settings\nstop - end the game\n";
 			}
 			else if (command == "next") {
 				break;
@@ -46,6 +49,9 @@ int main() {
 					wage = potentialWage2;
 					cout << "Changed the wage to " << wage << ".\n";
 				}
+			}
+			else if (command == "outsrc") {
+				cout << "Yessir";
 			}
 			else if (command == "stop") {
 				return 0;
