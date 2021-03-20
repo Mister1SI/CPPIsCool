9b6344ab15c72bbc068f9f94603778c58f28d8d7
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2021-03-20 10:00:11 -0500

    0.0.2: Add to program

--- CPPIsCool/Source.cpp
+++ CPPIsCool/Source.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
-#include 
+#include <string>
+using namespace std;
+int main() {
+	int workers = 100;
+	float totalCompanyValue = 100000.0;
+	string version = "0.0.2", name, companyName;
+	string randomEvents[] = { "deathAssassinated", "deathCancer" };
+	cout << "==========\nWelcome to Supply Chain. You are playing version: " + version + "\n\nPlease enter your name: \n> ";
+	cin >> name;
+	cout << "Thank you. Please enter your company name: \n> ";
+	cin >> companyName;
+	cout << "Press enter to begin.\n";
+	cout << "\n==========\nStart of month 1\nTotal workers: " << workers << endl << "\nTotal comany value: $" << totalCompanyValue;
+	return 0;
+};
